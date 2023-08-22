f8be5ea0626b51a037d6d81cd8507d893dbe29d3
andri <andriyparayko2017@gmail.com>
2023-08-21 18:21:29 -0700

    I finally fix git hub< and do tasks)

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -17,7 +18,7 @@ class Book(models.Model):
         return reverse("book_detail", args=[self.id])
 
     def save(self, *args, **kwargs):
-
+        self.slug = slugify(self.title)
         super().save(*args, **kwargs)
 
     def __str__(self):
