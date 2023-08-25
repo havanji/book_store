bcb1f40ab47036d1cab607c8c852cacd2bca9c6f
andri <andriyparayko2017@gmail.com>
2023-08-24 17:49:47 -0700

    vid98

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -12,7 +12,8 @@ class Book(models.Model):
         validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True) #primary_key=True
+    slug = models.SlugField(default="", blank=True, editable=False, 
+                            null=False, db_index=True) #primary_key=True
 
     def get_absolute_url(self):
         return reverse("book_detail", args=[self.slug])
